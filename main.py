7b26c0cfdd0a2077c1fe96e9287f53a6bd16f806
AkulChordia <acool8868@gmail.com>
2023-04-12 20:26:00 +0530

    8

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import os
 import openai
 import discord
 import datetime
+import asyncio
 from dotenv import load_dotenv
 
+reminder_mess = ""
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 openai.api_key = os.getenv('OPENAI_KEY')
 
 intents = discord.Intents.all()
-
 client = discord.Client(intents=intents)
+
+async def schedule_reminder(reminder_time, message1, message):
+    now = datetime.datetime.now()
+    delta = (reminder_time - now).total_seconds()
+    await asyncio.sleep(delta)
+    await message.channel.send("Reminder: It's now " + str(reminder_time)+"\n"+ str(' '.join(message1)))
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
@@ -21,7 +28,7 @@ async def on_message(message):
     channel = str(message.channel.name)
     user_message = str(message.content)
     if user_message.split(' ')[0].lower() == "help":
-        await message.channel.send("1. \'gpt\' + prompt\n2. \'rem\' + mm:hh (24 hour time) dd/mm/yyyy\n3. \'song\' + name")
+        await message.channel.send("1. \'gpt\' + prompt\n2. \'rem\' + mm:hh::dd/mm/yyyy + message\n3. \'song\' + name")
         return
     elif user_message.split(' ')[0].lower() == "gpt":
         await message.channel.send("[imagine chatgpt's reply here]")
@@ -32,7 +39,14 @@ async def on_message(message):
         await message.channel.send(get_response(superprompt))
         return
     elif user_message.split(' ')[0].lower() == "rem":
-
+        try:
+            reminder_time = datetime.datetime.strptime(user_message.split(' ')[1], '%H:%M::%d/%m/%Y')
+            reminder_mess = user_message.split(' ')[2::]
+            await message.channel.send("Reminder set for " + str(reminder_time))
+            await schedule_reminder(reminder_time, reminder_mess, message)
+        except ValueError:
+            await message.channel.send("Invalid reminder format. Please use the format \'rem HH:MM::DD/MM/YYYY (message)\'")
+        return
     elif user_message.split(' ')[0].lower() == "song":
         print("music")
 
@@ -50,5 +64,7 @@ def get_response(prompt):
     )
 
     return str(response.choices[0].text.strip())
+
+
 
 
