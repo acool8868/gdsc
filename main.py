52b869a0f092d3949f2a6389a0fc89a2ebeff61e
AkulChordia <acool8868@gmail.com>
2023-04-12 12:33:27 +0530

    4 (I'm not sure if this is how you use git)

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ async def on_ready():
     print(f'{client.user} is connected to the following guild:')
 
 client.run(TOKEN)
+
+
+def get_response(prompt):
+    model_engine = "text-davinci-002"
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    return response.choices[0].text.strip()
