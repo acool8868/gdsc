fc22042948d4e66deb0edb8c4cff781df8fb48e1
AkulChordia <acool8868@gmail.com>
2023-04-12 12:27:56 +0530

    3?

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import os
-
+import openai
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+openai.api_key = os.getenv('OPENAI_KEY')
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} is connected to the following guild:')
 
 client.run(TOKEN)
