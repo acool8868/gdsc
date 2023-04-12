816ca678e9410bee4c69bcce3cbfd2401ac5a0b6
AkulChordia <acool8868@gmail.com>
2023-04-12 16:09:34 +0530

    5

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import openai
 import discord
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,10 +13,20 @@ client = discord.Bot()
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
 
-client.run(TOKEN)
+@client.event
+async def on_message(message):
+    username = str(message.author).split("#")[0]
+    channel = str(message.channel.name)
+    user_message = str(message.content)
+    if user_message[0].lower() == "gpt":
+        print("reply")
+    elif user_message[0].lower() == "rem":
+        print("time")
+    elif user_message[0].lower() == "song":
+        print("music")
+client.run(token)
 
-
-def get_response(prompt):
+'''def get_response(prompt):
     model_engine = "text-davinci-002"
     response = openai.Completion.create(
         engine=model_engine,
@@ -26,5 +37,5 @@ def get_response(prompt):
         temperature=0.5,
     )
 
-    return response.choices[0].text.strip()
+    return response.choices[0].text.strip()'''
 
