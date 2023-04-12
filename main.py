a0656df3e08c1081cd06cab86ee2f821d00f1eb7
AkulChordia <acool8868@gmail.com>
2023-04-12 16:50:38 +0530

    6

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = "MTA5NTMyMjM5OTc3NzUwNTMyMQ.GqWWsK.qa30dsmlidrdvVEcX0iEKSnPBSrWTerFX9a8eM"
 openai.api_key = os.getenv('OPENAI_KEY')
-client = discord.Bot()
 
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print("Logged in as a bot {0.user}".format(client))
@@ -18,13 +21,18 @@ async def on_message(message):
     username = str(message.author).split("#")[0]
     channel = str(message.channel.name)
     user_message = str(message.content)
-    if user_message[0].lower() == "gpt":
-        print("reply")
-    elif user_message[0].lower() == "rem":
+
+    print(message)
+
+    if user_message.lower() == "gpt":
+        await message.channel.send("chat")
+        return
+    elif user_message.split(' ')[0].lower() == "rem":
         print("time")
-    elif user_message[0].lower() == "song":
+    elif user_message.split(' ')[0].lower() == "song":
         print("music")
-client.run(token)
+
+client.run(TOKEN)
 
 '''def get_response(prompt):
     model_engine = "text-davinci-002"
